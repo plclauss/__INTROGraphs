bbedf82bd9903452cce322b2df9effbd53862702
Paul Clauss <plclauss@gmail.com>
2022-08-16 16:32:54 -0500

    Graph class file created

---
+++ Graph.cpp
@@ -0,0 +1,5 @@
+//
+// Created by plcla on 8/16/2022.
+//
+
+#include "Graph.h"
---
+++ Graph.h
@@ -0,0 +1,14 @@
+//
+// Created by plcla on 8/16/2022.
+//
+
+#ifndef GRAPHSPRACTICE_GRAPH_H
+#define GRAPHSPRACTICE_GRAPH_H
+
+
+class Graph {
+
+};
+
+
+#endif //GRAPHSPRACTICE_GRAPH_H
